31f47d96fb9055e9e552296b06ae5b1c9c22e963
CLAW <christopher.lawn@yahoo.com>
2021-11-14 21:14:33 -0800

    created a loop to fight each robot at full health and fight each robot separately after the previous one died

--- assets/js/game.js
+++ assets/js/game.js
@@ -17,8 +17,8 @@ var enemyAttack = 12;
 // fight function
 var fight = function(enemyName) {
     // Alert players that they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
-
+    while(enemyHealth > 0) {
+        //window.alert("Welcome to Robot Gladiators");
     // ask players if they'd like to fight or run
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
     
@@ -68,9 +68,12 @@ var fight = function(enemyName) {
     } else {
         window.alert("You need to choose a valid option. Try again!");
     }
+    }
 };
 
 // run fight function to start game
 for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i])
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(enemyNames[i]);
 }
